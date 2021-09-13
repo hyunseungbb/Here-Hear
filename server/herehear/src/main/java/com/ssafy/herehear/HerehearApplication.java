0524e55f115fd1d1bde14578006176226fbd8557
오승철 <tmdcjf103@gmail.com>
2021-09-13 18:06:51 +0900

    feat: spring 서버 생성

---
+++ server/herehear/src/main/java/com/ssafy/herehear/HerehearApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.herehear;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerehearApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HerehearApplication.class, args);
+	}
+
+}
---
+++ server/herehear/src/test/java/com/ssafy/herehear/HerehearApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.herehear;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HerehearApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
