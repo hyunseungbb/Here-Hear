4054f72f5787c132195e68964339c7e69b2623a7
진지연 <serious_yeon@naver.com>
2021-09-15 15:32:23 +0900

    S05P21B105-403 refactor:AI_OCR/TTS 장고 셋팅

--- aiserver/apis/urls.py
+++ aiserver/apis/urls.py
@@ -4,6 +4,5 @@ from . import views
 app_name = 'apis'
 urlpatterns = [
     path('upload/', views.upload, name='upload'),
-    path('ocr/', views.ocr, name='ocr'),
-    path('tts/', views.tts, name='tts')
+    path('ocr_tts/', views.ocr_tts, name='ocr_tts'),
 ]
--- aiserver/apis/views.py
+++ aiserver/apis/views.py
@@ -1,16 +1,15 @@
-from django.shortcuts import render, redirect
 from gtts import gTTS
 import json
 import cv2
 import requests
 import sys
-from django.views.decorators.http import require_POST
-from .models import Article
-from .forms import ArticleForm
 import os
+from django.views.decorators.http import require_POST
 from django.core.files.storage import default_storage
 from django.core.files.base import ContentFile
 from django.conf import settings
+from django.shortcuts import render, redirect
+from django.http import JsonResponse 
 
 # dotenv 처리해서 보안 높이기
 appkey = '6ca73d4487a205586f13f1aebfe2f18a'
@@ -36,51 +35,41 @@ def test(image_path):
     return text, tts
 
 
-# # Create your views here.
-# @require_POST
-# def ocr(request):
-#     # POST일 때
-#     if request.method == 'POST':
-#         form = ArticleForm(request.POST, request.FILES)
-#         # form = ArticleForm(data=request.POST, files=request.FILES)
-#         if form.is_valid():
-#             article = form.save()
-#             return redirect('articles:detail', article.pk)
-
-
+# 장고 테스트용 프론트 (tmp)
 def upload(request):
     context = {'request':request}
     return render(request, 'apis/upload.html', context)
 
 
-def ocr(request):
-    # print('-----ocr tts 시작-----')
-    # print(request.FILES)
-
-    # print('-----imgs에 바로 읽기 > 알단 후퇴-----')
+@require_POST
+def ocr_tts(request):
+    # 폼으로 받은 데이터에 접근
     imgs = request.FILES.get('imgs').file
+
+    # 바로 읽기 : 일단 후퇴
     # image = cv2.imread(imgs)
     # print(image)
 
-    print('-----디스크에 저장-----')
+    # 디스크에 저장
     img_path = default_storage.save(f'tmp/{request.user}.jpg', ContentFile(imgs.read()))
     img_path2 = os.path.join(settings.MEDIA_ROOT, img_path)
 
-    print('-----ocr tts-----')
+    # ocr, tts
     ocr_result, tts_result = test(img_path2)
-    img_path3 = default_storage.delete(img_path)
+    img_path3 = default_storage.delete(img_path)    # ocr 파일은 불필요하므로 삭제
 
-    print('-----스피치 파일 저장하기-----')
+    # 스피치 파일 저장하기
     tts_path = f'/tmp/{request.user}.mp3'
     tts_result.save(f'{settings.MEDIA_ROOT}tts_path')
     
+    # json으로 넘겨줄 것
     context = {
         'ocr_result':ocr_result,
         'tts_result_url':'media'+tts_path
     }
     
-    return render(request, 'apis/ocr.html', context)
+    # 장고 테스트
+    # return render(request, 'apis/ocr.html', context)
+    # rest api 서버
+    return JsonResponse(context)
 
-
-def tts(request):
-    return render(request, 'apis/tts.html')
