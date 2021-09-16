b53d3364c67c332f559df4474f0476e514e66de6
오승철 <tmdcjf103@gmail.com>
2021-09-16 14:43:26 +0900

    S05P21B105-428 feat: django DB 연동

--- aiserver/aiserver/settings.py
+++ aiserver/aiserver/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'aiserver.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'herehear_db',
+        'USER': 'ssafy',
+        'PASSWORD': 'ssafy',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
