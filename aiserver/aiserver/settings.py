f475ba4e10278fef6f85a3cfa0a10adb05006dbe
진지연 <serious_yeon@naver.com>
2021-09-17 16:52:06 +0900

    docs : 협업필터링 코드 설명

--- aiserver/aiserver/settings.py
+++ aiserver/aiserver/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -79,7 +80,7 @@ WSGI_APPLICATION = 'aiserver.wsgi.application'
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'herehear_db',
+#         'NAME': 'herehear',
 #         'USER': os.environ.get("DB_USER"),
 #         'PASSWORD': os.environ.get("DB_PASSWORD"),
 #         'HOST': 'localhost',
