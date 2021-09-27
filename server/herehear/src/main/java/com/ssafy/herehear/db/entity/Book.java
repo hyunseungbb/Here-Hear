78834b2b794361b3f4569b37f01c48a4613a114b
오승철 <tmdcjf103@gmail.com>
2021-09-27 14:43:51 +0900

    S05P21B105-548 feat: Book 엔티티 수정

--- server/herehear/src/main/java/com/ssafy/herehear/db/entity/Book.java
+++ server/herehear/src/main/java/com/ssafy/herehear/db/entity/Book.java
@@ -21,19 +21,19 @@ public class Book extends BaseEntity {
 	private String description;	
 	private String publisher;	
 	private String img_url;	
-	private String vector;	
+//	private String vector;	
 	private int stars_sum;	
 	private int stars_count;
 	
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "tag_id")
-	private Tag tag;
-	
-	// 순환참조
-	@ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "par_book_id")
-    private Book parent;
-	
-	@OneToMany(fetch = FetchType.LAZY, mappedBy = "parent")
-	private List<Book> similar_book;
+//	@ManyToOne(fetch = FetchType.LAZY)
+//	@JoinColumn(name = "tag_id")
+//	private Tag tag;
+//	
+//	// 순환참조
+//	@ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "par_book_id")
+//    private Book parent;
+//	
+//	@OneToMany(fetch = FetchType.LAZY, mappedBy = "parent")
+//	private List<Book> similar_book;
 }
