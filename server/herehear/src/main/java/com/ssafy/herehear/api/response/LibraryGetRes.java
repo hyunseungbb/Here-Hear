e06c3057dcfa7a2013f54493246454d7b18e9017
whgywjd123 <whgywjd6794@gmail.com>
2021-09-28 14:39:53 +0900

    S05P21B105-546 feat: 내 서재 첵 목록 조회

--- server/herehear/src/main/java/com/ssafy/herehear/api/response/LibraryGetRes.java
+++ server/herehear/src/main/java/com/ssafy/herehear/api/response/LibraryGetRes.java
@@ -9,8 +9,14 @@ import lombok.Setter;
 @Setter
 @ApiModel("LibraryGetResponse")
 public class LibraryGetRes {
-	@ApiModelProperty(name="책 ID")
+	@ApiModelProperty(name="내 서재 ID")
 	private Long id;
+	
+	@ApiModelProperty(name="책 ID")
+	private Long book_id;
+	
+	@ApiModelProperty(name="유저 ID")
+	private Long user_id;
 	
 	@ApiModelProperty(name="읽음 상태", example = "전:0, 중:1, 후:2")
 	private int read_status;
