58ae88b04db246383fc11f59aaf10fd849b78b75
오승철 <tmdcjf103@gmail.com>
2021-09-30 15:09:37 +0900

    S05P21B105-558 feat: 나만의 감상평 조회 API

--- server/herehear/src/main/java/com/ssafy/herehear/api/controller/CommentController.java
+++ server/herehear/src/main/java/com/ssafy/herehear/api/controller/CommentController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,6 +20,7 @@ import com.ssafy.herehear.api.service.CommentService;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import springfox.documentation.annotations.ApiIgnore;
 
 @Api(value = "감상평 API", tags = {"Comment"})
 @RestController
@@ -39,6 +42,14 @@ public class CommentController {
 	@ApiOperation(value = "전체 감상평 조회")
 	public ResponseEntity<List<CommentGetRes>> getAllComment(@PathVariable(name = "bookId") Long bookId) {
 		List<CommentGetRes> commentList = commentService.getAllCommentOfBook(bookId);
+		return ResponseEntity.status(200).body(commentList);
+	}
+	
+	@GetMapping("/my/{bookId}")
+	@ApiOperation(value = "나의 감상평 조회")
+	public ResponseEntity<List<CommentGetRes>> gellAllMyComment(@PathVariable(name = "bookId") Long bookId, @ApiIgnore Authentication authentication) {
+		Long userId = Long.parseLong(authentication.getName());
+		List<CommentGetRes> commentList = commentService.getAllMyCommentOfBook(bookId, userId);
 		return ResponseEntity.status(200).body(commentList);
 	}
 }
