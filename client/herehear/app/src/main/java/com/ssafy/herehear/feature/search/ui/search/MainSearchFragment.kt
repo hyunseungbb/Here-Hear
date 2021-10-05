425283832c5fd2f03d03688074ec81c296e55dff
slovecoms <gloveomg@gmail.com>
2021-10-05 11:40:48 +0900

    pull

--- client/herehear/app/src/main/java/com/ssafy/herehear/feature/search/ui/search/MainSearchFragment.kt
+++ client/herehear/app/src/main/java/com/ssafy/herehear/feature/search/ui/search/MainSearchFragment.kt
@@ -66,7 +66,7 @@ class MainSearchFragment : Fragment() {
             }
         }
 
-        val url = "http://image.aladin.co.kr/product/10789/58/cover/8964359208_1.jpg"
+        var url = ""
         Glide.with(requireContext()).load(url).override(150, 200).into(binding.imageView2)
 
         // 검색바에서 텍스트 입력시 수행하는 함수
@@ -86,7 +86,7 @@ class MainSearchFragment : Fragment() {
                         if (response.isSuccessful){
                             // need to modify
                             // To print book image
-                            response.body()?.let { loadData(url)}
+                            url = response.body()?.imageUrl.toString()
 //                            // 책 이미지 클릭시 goInfo 로 책정보 화면으로 가기
 //                            binding.bookImage.setOnClickListener{
 //                                (parentFragment as SearchFragment).goInfo()
