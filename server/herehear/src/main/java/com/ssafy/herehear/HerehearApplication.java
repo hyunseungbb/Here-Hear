ef604d2299e95517b2857ef25ac1d995c50cb72b
양현승 <happyman1104@naver.com>
2022-01-11 16:05:55 +0900

    fix 설정 수정

--- server/herehear/src/main/java/com/ssafy/herehear/HerehearApplication.java
+++ server/herehear/src/main/java/com/ssafy/herehear/HerehearApplication.java
@@ -2,14 +2,19 @@ package com.ssafy.herehear;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableCaching
+//@EnableJpaAuditing
 @SpringBootApplication
+//@EntityScan(basePackages="com.ssafy.herehear")
 public class HerehearApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HerehearApplication.class, args);
 	}
-
 }
