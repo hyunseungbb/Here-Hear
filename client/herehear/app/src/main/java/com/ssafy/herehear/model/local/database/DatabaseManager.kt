af0cf49ae10a786ae8748bf3fe1a68c319253101
양현승 <happyman1104@naver.com>
2022-02-21 15:22:10 +0900

    room interface & library dao

--- client/herehear/app/src/main/java/com/ssafy/herehear/model/local/database/DatabaseManager.kt
+++ client/herehear/app/src/main/java/com/ssafy/herehear/model/local/database/DatabaseManager.kt
@@ -1,4 +1,11 @@
 package com.ssafy.herehear.model.local.database
 
-class DatabaseManager {
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.ssafy.herehear.model.local.dao.LibraryDao
+import com.ssafy.herehear.model.local.entity.Library
+
+@Database(entities = [Library::class], version = 1)
+abstract class DatabaseManager: RoomDatabase() {
+    abstract val libraryDao: LibraryDao
 }
--- client/herehear/app/src/main/java/com/ssafy/herehear/model/local/entity/Library.kt
+++ client/herehear/app/src/main/java/com/ssafy/herehear/model/local/entity/Library.kt
@@ -1,14 +1,26 @@
-package com.ssafy.herehear.model.data.local.entity
+package com.ssafy.herehear.model.local.entity
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.ssafy.herehear.model.local.entity.Library.Companion.TABLE_NAME
 
-@Entity(tableName = "Library")
+@Entity(tableName = TABLE_NAME)
 data class Library(
     @PrimaryKey
+    val table_id: String,
     val id: String,
     val book_id: Int,
     val img_url: String,
     val read_status: Int,
     val stars: Int
-)
+) {
+    companion object {
+        const val TABLE_NAME = "Library"
+        const val COLUMN_TABLE_ID = "table_id"
+        const val COLUMN_LIBRARY_ID = "id"
+        const val COLUMN_BOOK_ID = "book_id"
+        const val COLUMN_IMG_URL = "img_url"
+        const val COLUMN_READ_STATUS = "read_status"
+        const val COLUMN_START = "stars"
+    }
+}
